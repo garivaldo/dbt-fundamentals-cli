c6e0f18b05fed9dda35b2c868920bbf0617d00a6
Gary Tucker <garivaldo@users.noreply.github.com>
2020-12-30 16:01:09 -0500

    Jinja Skills

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -4,7 +4,7 @@ with payments as (
   orderid as order_id,
   paymentmethod,
   status as payment_status,
-  amount/100 as payment_amount,
+  {{ cents_to_dollars('payment_amount') }} as payment_amount,
   created as created_at
     from {{source('stripe','payment')}}
   )
